38034b3b38024285f7884e8c64a2a07c2834889b
Manoel Costa <mnfcosta1@gmail.com>
2023-02-16 10:33:27 -0300

    adicionando django messages e cadastro de usuário

--- login/views.py
+++ login/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
@@ -9,6 +11,24 @@ def signin(request):
     return render(request, "login/pages/signin.html")
 
 def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        cpassword = request.POST.get("cpassword")
+        
+        user = User.objects.create_user(username, email, password)
+        user.first_name = fname
+        user.last_name = lname
+
+        user.save()
+
+        messages.success(request, "Conta criada com sucesso!")
+
+        return redirect("login:signin")
+
     return render(request, "login/pages/register.html")
 
 def signout(request):
