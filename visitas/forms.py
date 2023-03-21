d2002ec64f79da4d0cfc57fc2ce4edd4e3a2b6ab
Manoel Costa <mnfcosta1@gmail.com>
2023-03-21 08:51:29 -0300

    ..

--- visitas/forms.py
+++ visitas/forms.py
@@ -11,15 +11,3 @@ class ParticipantesForm(forms.ModelForm):
     class Meta:
         model = VisitaParticipantes
         exclude = ['visita']
-    """ def save(self, commit=True):
-        #chama o método save padrão para criar uma instancia do model
-        #porém commit=False para que a instancia nao seja salva na db
-        instance = super().save(commit=False)
-        if commit:
-            instance.save()  # salva a instancia na db com valores para que ela tenha um id 
-            #(necessário para salvar multiselectfield)
-        instance.familia.set(id)
-        instance.participantes.set(self.cleaned_data['participantes']) #coloca os dados do formulário no atributo componentes do model
-        if commit:
-            instance.save() #salva a instancia com todos os dados na db
-        return instance """
