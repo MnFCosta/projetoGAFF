075ef707fd120336ed4d2ee4c517430663de3a93
Manoel Costa <mnfcosta1@gmail.com>
2023-03-22 10:34:32 -0300

    adicionando model de item e intens da entrega

--- entregas/admin.py
+++ entregas/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from entregas.models import *
+import math
 # Register your models here.
 
 class EntregaAdmin(admin.ModelAdmin):
@@ -8,3 +9,27 @@ class EntregaAdmin(admin.ModelAdmin):
     search_fields = ('id','familia', 'data_entrega')
 
 admin.site.register(Entrega, EntregaAdmin)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'codigo_barras', 'estoque_display')
+    list_display_links = ('id','nome', 'codigo_barras', 'estoque_display')
+    search_fields = ('id','nome', 'codigo_barras', 'estoque_display')
+
+    def estoque_display(self, obj):
+        return f"{math.trunc(obj.estoque_atual)}g" 
+    
+    estoque_display.short_description = 'Em estoque' # type: ignore
+
+admin.site.register(Item, ItemAdmin)
+
+class EntregaItensAdmin(admin.ModelAdmin):
+    list_display = ('id','item', 'entrega', 'quantidade_display')
+    list_display_links = ('id','item', 'entrega', 'quantidade_display')
+    search_fields = ('id','item', 'entrega', 'quantidade_display')
+
+    def quantidade_display(self, obj):
+        return f"{math.trunc(obj.quantidade)}g"
+    
+    quantidade_display.short_description = 'Quantidade' # type: ignore
+
+admin.site.register(ItemEntrega, EntregaItensAdmin)
--- entregas/models.py
+++ entregas/models.py
@@ -12,3 +12,30 @@ class Entrega(models.Model):
     class Meta:
         verbose_name = "Entrega"
         verbose_name_plural = "Entregas"
+
+class Item(models.Model):
+    nome = models.CharField(max_length=200, unique=True)
+    unidade = models.CharField(max_length=45)
+    multiplicador = models.DecimalField(max_digits=20, decimal_places=2)
+    codigo_barras = models.CharField(max_length=100)
+    estoque_atual = models.DecimalField(max_digits=20, decimal_places=2)
+    observacao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Itens"
+
+class ItemEntrega(models.Model):
+    entrega = models.ForeignKey(Entrega, verbose_name="Entrega", on_delete=CASCADE)
+    item = models.ForeignKey(Item, verbose_name="Item da entrega", on_delete=CASCADE)
+    quantidade = models.DecimalField(max_digits=20, decimal_places=2)
+    
+    def __str__(self):
+        return self.item.nome
+
+    class Meta:
+        verbose_name = "Item da entrega"
+        verbose_name_plural = "Itens de entregas"
