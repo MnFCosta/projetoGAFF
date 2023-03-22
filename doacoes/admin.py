d61f3bccee0cef5868a1fc98a6f77baf47e2b14b
Manoel Costa <mnfcosta1@gmail.com>
2023-03-22 11:16:45 -0300

    adicionando models de itens de doação

--- doacoes/admin.py
+++ doacoes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+import math
 
 # Register your models here.
 class DoadorAdmin(admin.ModelAdmin):
@@ -8,3 +9,22 @@ class DoadorAdmin(admin.ModelAdmin):
     search_fields = ('id','nome', 'celular', 'bairro', 'rua', 'numero', 'cidade', 'unidade_federativa')
     
 admin.site.register(Doador, DoadorAdmin)
+
+class DoacoesAdmin(admin.ModelAdmin):
+    list_display = ('id','doador', 'data_doacao')
+    list_display_links = ('id','doador', 'data_doacao')
+    search_fields =('id','doador', 'data_doacao')
+    
+admin.site.register(Doacao, DoacoesAdmin)
+
+class DoacaoItensAdmin(admin.ModelAdmin):
+    list_display = ('id','item', 'doacao', 'quantidade_display')
+    list_display_links = ('id','item', 'doacao', 'quantidade_display')
+    search_fields = ('id','item', 'doacao', 'quantidade_display')
+
+    def quantidade_display(self, obj):
+        return f"{math.trunc(obj.quantidade)}g"
+    
+    quantidade_display.short_description = 'Quantidade' # type: ignore
+
+admin.site.register(ItemDoacao, DoacaoItensAdmin)
