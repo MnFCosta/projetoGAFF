b495083ab9de52eb3be906668c446c1a3b48feb7
Manoel Costa <mnfcosta1@gmail.com>
2023-03-23 09:58:00 -0300

    adicionando view e templates para doacao

--- doacoes/urls.py
+++ doacoes/urls.py
@@ -6,7 +6,10 @@ from doacoes import views
 app_name = 'doacoes'
 
 urlpatterns = [
-    path('doacoes', views.doacoes, name="doacoes")
+    path('doacoes', views.doacoes, name="doacoes"), 
+    path('doacoes/<int:id>/', views.doacaoDetail, name="doacao_detail"),
+    path('cadastro_doacao', views.cadastroDoacao, name="cadastro_doacao"),
+    path('cadastro_itens/<int:id>', views.itensDoacao, name="cadastro_itens")
     
 
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -5,7 +5,7 @@ from .models import Movimentacao
 # Create your views here.
 def estoque(request):
     itens = Item.objects.order_by("id")
-    movimentacoes = Movimentacao.objects.order_by("id")
+    movimentacoes = Movimentacao.objects.order_by("-id")
     tipo = movimentacoes[1].tipo
     print(tipo.model)
     
